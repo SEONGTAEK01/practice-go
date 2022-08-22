39231f03d60ea55241a2de4b2cbf6e631422f102
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2022-08-22 11:05:37 +0900

    Add web-app main, view page handlers

---
+++ src/web-app/main.go
@@ -0,0 +1,53 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	p, _ := loadPage("index")
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("Hello!")}
+	p1.save()
+
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
